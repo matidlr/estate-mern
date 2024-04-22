ddebed58a3dcf33f4d4835ff198990e7a4db7631
Matias <mati.delarosa1984@gmail.com>
2024-04-22 14:46:37 -0300

    fix tailwind

--- MERN/client/src/App.jsx
+++ MERN/client/src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react' 
-
-const App = () => {
+export default function App() {
   return (
-    <div className='text-red-500'>App</div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
