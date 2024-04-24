d46c68f7148e0fe329f5b5ec264c2aa7951ba5de
Matias <mati.delarosa1984@gmail.com>
2024-04-24 12:07:39 -0300

    add redux toolkit

--- MERN/client/src/pages/SignIn.jsx
+++ MERN/client/src/pages/SignIn.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
 
 const SignIn = () => {
  const [formData, setFormData] = useState({});
- const [error, setError] = useState(null);
- const [loading, setLoading] = useState(false);
+const { loading, error } = useSelector((state) => state.user);
  const navigate = useNavigate();
  const handleChange = (e) => {
     setFormData({
@@ -16,7 +17,7 @@ const SignIn = () => {
  const handleSubmit = async (e) => {
    e.preventDefault();
   try {
-    setLoading(true);
+    dispatch(signInStart());
     const res = await fetch('/api/auth/signin', {
      method: 'POST',
      headers: {
@@ -26,16 +27,13 @@ const SignIn = () => {
     });
     const data = await res.json();
     if(data.success === false) {
-      setLoading(false);
-     setError(data.message);
+      dispatch(signInFailure(data.message));
      return;
     }
-    setLoading(false);
-    setError(null);
+    dispatch(signInSuccess(data));
     navigate('/');
   } catch (error) {
-    setLoading(false);
-    setError(error.message);
+    dispatch(signInFailure(error.message));
   }
   };
 
