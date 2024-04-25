fb4f26c3d8c7e8499d6da390e1361a272570ed7d
Matias <mati.delarosa1984@gmail.com>
2024-04-24 21:42:11 -0300

    create and add OAuth component

--- MERN/api/models/user.module.js
+++ MERN/api/models/user.module.js
@@ -14,7 +14,11 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-  }
+  },
+  avatar:{
+    type: String,
+    default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw3Ymw0GtYgn9shTNMqRcOE6&ust=1714090606857000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJCYo6-L3IUDFQAAAAAdAAAAABAE",
+  },
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
