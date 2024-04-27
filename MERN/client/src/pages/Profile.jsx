05431abb832b08fa41c6eb7f5a38160cabef54f6
Matias <mati.delarosa1984@gmail.com>
2024-04-27 00:27:01 -0300

    add signout functionality

--- MERN/client/src/pages/Profile.jsx
+++ MERN/client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import {useRef, useState, useEffect} from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable, } from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserSuccess, signOutUserFailure } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -95,6 +95,21 @@ const handleDeleteUser = async () =>{
   }
 }
 
+const handleSignOut = async () => {
+  try {
+    dispatch(signOutUserStart())
+    const res = await fetch('/api/auth/signout');
+    const data = await res.json();
+    if (data.success === false) {
+    dispatch(deleteUserFailure(data.message));
+      return;
+    }
+    dispatch(deleteUserSuccess(data));
+  } catch (error) {
+    dispatch(deleteUserFailure(data.message));
+  }
+}
+
   return (
     <div className="p-3 max-w-lg mx-auto">
        <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -157,8 +172,13 @@ const handleDeleteUser = async () =>{
        </form>
        <div className="flex justify-between mt-5">
          <span onClick={handleDeleteUser}
-         className="text-red-700 cursor-pointer">Delete account</span>
-         <span className="text-red-700 cursor-pointer">Sign out</span>
+         className="text-red-700 cursor-pointer">
+            Delete account
+          </span>
+          <span onClick={handleSignOut}
+             className="text-red-700 cursor-pointer">
+              Sign out
+           </span>
        </div>
        <p className="text-red-700 mt-5">{error ? error : ''}</p>
        <p className="text-green-700 mt-5">
