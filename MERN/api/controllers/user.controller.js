0980ed2894ba6e233ab7e1820f17d9dfce57cad9
Matias <mati.delarosa1984@gmail.com>
2024-04-29 12:54:13 -0300

    create get user listings api route

--- MERN/api/controllers/user.controller.js
+++ MERN/api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.module.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import Listings from '../models/listing.model.js'
 
 export const test = (req, res) => {
   res.json({
@@ -46,4 +47,17 @@ export const deleteUser = async (req, res, next)=> {
   } catch (error) {
     next(error)
   }
+}
+
+export const getUserListings = async (req, res, next) => {
+   if(req.user.id === req.params.id){
+      try {
+        const listings = await Listings.find({ useRef: req.params.id });
+        res.status(200).json(listings)
+      } catch (error) {
+        next(error);
+      }
+   }else{
+    return next(errorHandler(401, 'You can only view your own listings!'))
+   }
 }
