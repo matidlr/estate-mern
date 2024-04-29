fe336e8f5ea29a7d72ed3b3e28c12bc7e02c2cca
Matias <mati.delarosa1984@gmail.com>
2024-04-29 15:44:39 -0300

    delete listing route and controller

--- MERN/api/models/user.module.js
+++ MERN/api/models/user.module.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
   },
   avatar:{
     type: String,
-    default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw3Ymw0GtYgn9shTNMqRcOE6&ust=1714090606857000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJCYo6-L3IUDFQAAAAAdAAAAABAE",
+    default: "https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small_2x/user-profile-icon-free-vector.jpg",
   },
 }, { timestamps: true });
 
