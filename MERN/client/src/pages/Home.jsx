404b7cdad3cdfc2de23b39e4a249a3b50ec23201
Matias <mati.delarosa1984@gmail.com>
2024-05-03 11:45:23 -0300

    fix errors

--- MERN/client/src/pages/Home.jsx
+++ MERN/client/src/pages/Home.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      {/* top */}
+      <div className="">
+      <h1>
+        Find your next <span>perfect</span>
+        <br/>
+        place with ease
+      </h1>
+      <div className="">
+        This Real Estate site is the best place to find your next perfect place to live.
+        <br/>
+        We have a wide range of properties for you to choose from
+      </div>
+      <Link to={'/search'}>
+        Let's get started
+      </Link>
+      </div>
+    </div>
   )
 }
 
