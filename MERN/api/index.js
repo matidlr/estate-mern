fdee98384d048095d63312e275abe760308e3f86
Matias <mati.delarosa1984@gmail.com>
2024-05-09 13:33:10 -0300

    port

--- MERN/api/index.js
+++ MERN/api/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(4000, () => {
+app.listen('https://front-estate-mdlr.netlify.app/' || 4000, () => {
   console.log('Server is running on port 3000!');
 });
 
