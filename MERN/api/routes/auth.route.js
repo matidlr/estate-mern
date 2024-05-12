417ab583487a5105bb724b13935233e00572ad80
Matias <mati.delarosa1984@gmail.com>
2024-05-12 16:09:02 -0300

    route

--- MERN/api/routes/auth.route.js
+++ MERN/api/routes/auth.route.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import express from 'express';
 import { google, signOut, signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
